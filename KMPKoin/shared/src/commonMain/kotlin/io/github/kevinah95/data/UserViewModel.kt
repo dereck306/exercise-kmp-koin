3331e4d1e0b0a0185c60f63fa8ccc1a672b20eb6
Dereck Rojas <dereckrm2000@gmail.com>
2025-07-22 20:57:28 -0600

    Step 5

--- KMPKoin/shared/src/commonMain/kotlin/io/github/kevinah95/data/UserViewModel.kt
+++ KMPKoin/shared/src/commonMain/kotlin/io/github/kevinah95/data/UserViewModel.kt
@@ -1,8 +1,8 @@
 package io.github.kevinah95
 
 import io.github.kevinah95.data.UserRepository
+import androidx.lifecycle.ViewModel
 
-class UserViewModel(private val userRepository: UserRepository) {
+class UserViewModel(private val userRepository: UserRepository) : ViewModel() {
     fun getGreeting(): String = "Hello, ${userRepository.getUsers().firstOrNull()?.name ?: "Guest"}!"
-    fun getUsers() = userRepository.getUsers()
 }
